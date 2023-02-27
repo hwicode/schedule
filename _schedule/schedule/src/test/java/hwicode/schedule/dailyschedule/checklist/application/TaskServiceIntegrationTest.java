a172af9a1cffd818b0873c52afbcca4de4838925
hwicode <khc3796@naver.com>
2023-02-27 15:47:02 +0900

    Refactor: EntityManager를 가져올 때 애노테이션을 @PersistenceContext로 변경
    - @PersistenceContext는 모든 스레드에 고유한 EntityManager를 생성한다.
    - 스프링 컨테이너는 싱글톤이므로 @Autowired를 사용하면 동시성 문제가 발생한다.

--- _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/checklist/application/TaskServiceIntegrationTest.java
+++ _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/checklist/application/TaskServiceIntegrationTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,7 +25,7 @@ public class TaskServiceIntegrationTest {
     @Autowired
     DailyChecklistRepository dailyChecklistRepository;
 
-    @Autowired
+    @PersistenceContext
     EntityManager entityManager;
 
     @Test
