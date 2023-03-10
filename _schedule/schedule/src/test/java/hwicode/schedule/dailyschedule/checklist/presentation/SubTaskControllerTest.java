6546c2718f4b407bc9b27c0d1b3b14d0c0d4384a
hwicode <khc3796@naver.com>
2023-03-10 16:31:54 +0900

    Test: SubTask 관련된 에러가 발생할 때 테스트 작성 - #20

--- _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/checklist/presentation/SubTaskControllerTest.java
+++ _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/checklist/presentation/SubTaskControllerTest.java
@@ -3,6 +3,8 @@ package hwicode.schedule.dailyschedule.checklist.presentation;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hwicode.schedule.dailyschedule.checklist.application.SubTaskService;
 import hwicode.schedule.dailyschedule.checklist.domain.Status;
+import hwicode.schedule.dailyschedule.checklist.exception.task.SubTaskNameDuplicationException;
+import hwicode.schedule.dailyschedule.checklist.exception.task.SubTaskNotFoundException;
 import hwicode.schedule.dailyschedule.checklist.presentation.subtask_dto.delete.SubTaskDeleteRequest;
 import hwicode.schedule.dailyschedule.checklist.presentation.subtask_dto.save.SubTaskSaveRequest;
 import hwicode.schedule.dailyschedule.checklist.presentation.subtask_dto.status_modify.SubTaskStatusModifyRequest;
@@ -87,5 +89,37 @@ public class SubTaskControllerTest {
         verify(subTaskService).changeSubTaskStatus(any(), any());
     }
 
-}
+    @Test
+    void 서브_과제_생성을_요청할_때_이름이_중복되면_에러가_발생한다() throws Exception {
+        // given
+        SubTaskNameDuplicationException subTaskNameDuplicationException = new SubTaskNameDuplicationException();
+        given(subTaskService.saveSubTask(any()))
+                .willThrow(subTaskNameDuplicationException);
 
+        // when then
+        mockMvc.perform(post("/subtasks")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new SubTaskSaveRequest())))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(subTaskNameDuplicationException.getMessage()));
+
+        verify(subTaskService).saveSubTask(any());
+    }
+
+    @Test
+    void 서브_과제를_찾을_때_서브_과제가_존재하지_않으면_에러가_발생한다() throws Exception {
+        // given
+        SubTaskNotFoundException subTaskNotFoundException = new SubTaskNotFoundException();
+        given(subTaskService.changeSubTaskStatus(any(), any()))
+                .willThrow(subTaskNotFoundException);
+
+        // when then
+        mockMvc.perform(patch("/subtasks/subTaskName/status")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new SubTaskStatusModifyRequest())))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value(subTaskNotFoundException.getMessage()));
+
+        verify(subTaskService).changeSubTaskStatus(any(), any());
+    }
+}
