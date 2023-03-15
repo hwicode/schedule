8603bb573fdc5f11a9e59bf781f12b934933a574
hwicode <khc3796@naver.com>
2023-03-15 15:20:57 +0900

    Feature: GlobalExceptionHandler에서 handleHttpMessageNotReadable 메서드 오버라이드함  - #22
    - handleHttpMessageNotReadable의 경우 리턴하는 에러 메시지에 서버 정보도 나오고 너무 상세하여, 간단한 메시지가 나오도록 오버라이드함

--- _schedule/schedule/src/main/java/hwicode/schedule/dailyschedule/checklist/exception/exception_handler/GlobalErrorCode.java
+++ _schedule/schedule/src/main/java/hwicode/schedule/dailyschedule/checklist/exception/exception_handler/GlobalErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum GlobalErrorCode {
 
-    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "유효하지 않는 파라미터가 있습니다.");
+    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "유효하지 않는 파라미터가 있습니다."),
+    INVALID_MESSAGE_BODY_TYPE(HttpStatus.BAD_REQUEST, "HTTP message body 중에 타입을 잘못 설정한게 있습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
--- _schedule/schedule/src/main/java/hwicode/schedule/dailyschedule/checklist/exception/exception_handler/GlobalExceptionHandler.java
+++ _schedule/schedule/src/main/java/hwicode/schedule/dailyschedule/checklist/exception/exception_handler/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import hwicode.schedule.dailyschedule.checklist.exception.exception_handler.Erro
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -27,6 +28,13 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
         return ResponseEntity.status(status)
                 .body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        GlobalErrorCode globalErrorCode = GlobalErrorCode.INVALID_MESSAGE_BODY_TYPE;
+        return ResponseEntity.status(status)
+                .body(new ErrorResponse(globalErrorCode.getMessage()));
     }
 
     @Override
