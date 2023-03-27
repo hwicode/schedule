b851d291f23a265f4001d738dda057ca6925f811
hwicode <khc3796@naver.com>
2023-03-27 10:43:38 +0900

    Comment : SaveOnlyRepository는 @Repository 애노테이션을 필요로 하지 않기 때문에 제거함 - #25

--- _schedule/schedule/src/main/java/hwicode/schedule/dailyschedule/checklist/domain/SubTaskSaveOnlyRepository.java
+++ _schedule/schedule/src/main/java/hwicode/schedule/dailyschedule/checklist/domain/SubTaskSaveOnlyRepository.java
@@ -1,8 +1,5 @@
 package hwicode.schedule.dailyschedule.checklist.domain;
 
-import org.springframework.stereotype.Repository;
-
-@Repository
 public interface SubTaskSaveOnlyRepository {
     SubTask save(SubTask subTask);
 }
--- _schedule/schedule/src/main/java/hwicode/schedule/dailyschedule/checklist/domain/TaskSaveOnlyRepository.java
+++ _schedule/schedule/src/main/java/hwicode/schedule/dailyschedule/checklist/domain/TaskSaveOnlyRepository.java
@@ -1,8 +1,5 @@
 package hwicode.schedule.dailyschedule.checklist.domain;
 
-import org.springframework.stereotype.Repository;
-
-@Repository
 public interface TaskSaveOnlyRepository {
     Task save(Task task);
 }
