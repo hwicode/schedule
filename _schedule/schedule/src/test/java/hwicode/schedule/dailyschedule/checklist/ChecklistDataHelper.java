e64733d96c3047b0452b9f3c36186f848226b1d6
hwicode <khc3796@naver.com>
2023-03-28 10:55:01 +0900

    Feature: ChecklistDataHelper 클래스 생성 - #26
    - checklist 패키지 내의 테스트들이 사용하는 데이터를 제공하는 클래스 생성
    - 역할 -> checklist 패키지 내의 테스트들에서 발생하는 데이터 중복 제거

---
+++ _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/checklist/ChecklistDataHelper.java
@@ -0,0 +1,18 @@
+package hwicode.schedule.dailyschedule.checklist;
+
+public class ChecklistDataHelper {
+
+    public static final Long DAILY_CHECKLIST_ID = 1L;
+    public static final Long TASK_ID = 2L;
+    public static final Long SUB_TASK_ID = 3L;
+
+    public static final String TASK_NAME = "taskName";
+    public static final String TASK_NAME2 = "taskName2";
+    public static final String TASK_NAME3 = "taskName3";
+
+    public static final String SUB_TASK_NAME = "subTaskName";
+    public static final String SUB_TASK_NAME2 = "subTaskName2";
+
+    public static final String NEW_TASK_NAME = "newTaskName";
+    public static final String NEW_SUB_TASK_NAME = "newSubTaskName";
+}
