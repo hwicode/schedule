6bf7cd5c799a0364c0f8e58297a21a59272bcb92
hwicode <khc3796@naver.com>
2023-03-29 08:50:03 +0900

    Refactor: DailyChecklistFindServiceTest가 id값을 가지도록 수정함 -#26
    - id값의 주인이 DailyChecklistFindServiceTest라고 생각되어 수정함
    - 테스트에 명확히 표현되어야 이해하기 더욱 쉬울 것으로 생각함

--- _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/checklist/application/DailyChecklistFindServiceTest.java
+++ _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/checklist/application/DailyChecklistFindServiceTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static hwicode.schedule.dailyschedule.checklist.ChecklistDataHelper.DAILY_CHECKLIST_ID;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
@@ -27,8 +26,11 @@ public class DailyChecklistFindServiceTest {
 
     @Test
     public void 존재하지_않는_체크리스트를_조회하면_에러가_발생한다() {
+        //given
+        Long noneExistId = 1L;
+
         // when then
-        assertThatThrownBy(() -> DailyChecklistFindService.findDailyChecklistWithTasks(dailyChecklistRepository, DAILY_CHECKLIST_ID))
+        assertThatThrownBy(() -> DailyChecklistFindService.findDailyChecklistWithTasks(dailyChecklistRepository, noneExistId))
                 .isInstanceOf(DailyChecklistNotFoundException.class);
     }
 }
