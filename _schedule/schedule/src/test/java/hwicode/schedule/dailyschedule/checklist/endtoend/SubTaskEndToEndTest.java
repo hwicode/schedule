b94355b6f034018197e5d8eb327f00a7dd00da16
hwicode <khc3796@naver.com>
2023-03-28 09:15:02 +0900

    Refactor: endtoend 패키지 생성 및 end to end 테스트 옮김 - #26

--- _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/checklist/endtoend/SubTaskEndToEndTest.java
+++ _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/checklist/endtoend/SubTaskEndToEndTest.java
@@ -1,5 +1,6 @@
-package hwicode.schedule.dailyschedule.checklist;
+package hwicode.schedule.dailyschedule.checklist.endtoend;
 
+import hwicode.schedule.dailyschedule.checklist.DatabaseCleanUp;
 import hwicode.schedule.dailyschedule.checklist.application.SubTaskService;
 import hwicode.schedule.dailyschedule.checklist.application.TaskService;
 import hwicode.schedule.dailyschedule.checklist.domain.DailyChecklist;
