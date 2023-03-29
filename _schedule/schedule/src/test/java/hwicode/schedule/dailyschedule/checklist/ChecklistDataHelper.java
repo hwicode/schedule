6a55efb66e5ff07a5ae6a5f6e89f5b176156d90e
hwicode <khc3796@naver.com>
2023-03-29 11:19:15 +0900

    Feature: ChecklistDataHelper 클래스에 checklist 패키지 내에 있는 request dto를 생성하는 메서드 만듦 -#26
    - 컨트롤러에서 사용되는 request dto의 경우 변경될 가능성이 크기 때문에, request dto가 변하더라도 테스트에서 한 곳에서만 수정하도록 만듦

--- _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/checklist/ChecklistDataHelper.java
+++ _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/checklist/ChecklistDataHelper.java
@@ -1,5 +1,15 @@
 package hwicode.schedule.dailyschedule.checklist;
 
+import hwicode.schedule.dailyschedule.checklist.domain.Difficulty;
+import hwicode.schedule.dailyschedule.checklist.domain.Status;
+import hwicode.schedule.dailyschedule.checklist.presentation.subtask_dto.delete.SubTaskDeleteRequest;
+import hwicode.schedule.dailyschedule.checklist.presentation.subtask_dto.save.SubTaskSaveRequest;
+import hwicode.schedule.dailyschedule.checklist.presentation.subtask_dto.status_modify.SubTaskStatusModifyRequest;
+import hwicode.schedule.dailyschedule.checklist.presentation.task_dto.delete.TaskDeleteRequest;
+import hwicode.schedule.dailyschedule.checklist.presentation.task_dto.difficulty_modify.TaskDifficultyModifyRequest;
+import hwicode.schedule.dailyschedule.checklist.presentation.task_dto.save.TaskSaveRequest;
+import hwicode.schedule.dailyschedule.checklist.presentation.task_dto.status_modify.TaskStatusModifyRequest;
+
 public class ChecklistDataHelper {
 
     public static final Long DAILY_CHECKLIST_ID = 1L;
@@ -15,4 +25,34 @@ public class ChecklistDataHelper {
 
     public static final String NEW_TASK_NAME = "newTaskName";
     public static final String NEW_SUB_TASK_NAME = "newSubTaskName";
+
+    // checklist request dto
+    public static TaskSaveRequest createTaskSaveRequest(Long dailyChecklistId, String taskName) {
+        return new TaskSaveRequest(dailyChecklistId, taskName);
+    }
+
+    public static TaskStatusModifyRequest createTaskStatusModifyRequest(Long dailyChecklistId, Status status) {
+        return new TaskStatusModifyRequest(dailyChecklistId, status);
+    }
+
+    public static TaskDifficultyModifyRequest createTaskDifficultyModifyRequest(Long dailyChecklistId, Difficulty difficulty) {
+        return new TaskDifficultyModifyRequest(dailyChecklistId, difficulty);
+    }
+
+    public static TaskDeleteRequest createTaskDeleteRequest(Long dailyChecklistId) {
+        return new TaskDeleteRequest(dailyChecklistId);
+    }
+
+    public static SubTaskSaveRequest createSubTaskSaveRequest(Long dailyChecklistId, String taskName, String subTaskName) {
+        return new SubTaskSaveRequest(dailyChecklistId, taskName, subTaskName);
+    }
+
+    public static SubTaskStatusModifyRequest createSubTaskStatusModifyRequest(Long dailyChecklistId, String taskName, Status status) {
+        return new SubTaskStatusModifyRequest(dailyChecklistId, taskName, status);
+    }
+
+    public static SubTaskDeleteRequest createSubTaskDeleteRequest(Long dailyChecklistId, String taskName) {
+        return new SubTaskDeleteRequest(dailyChecklistId, taskName);
+    }
+
 }
