14724982d36bb4ffaf448aa6740a1c12aba31543
hwicode <khc3796@naver.com>
2023-03-29 16:22:03 +0900

    Comment : ChecklistDataHelper클래스의 데이터 사용 조건을 주석으로 작성 -#26

--- _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/checklist/ChecklistDataHelper.java
+++ _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/checklist/ChecklistDataHelper.java
@@ -17,10 +17,13 @@ import hwicode.schedule.dailyschedule.checklist.presentation.task_dto.status_mod
 
 public class ChecklistDataHelper {
 
+    // 단순히 id값으로 숫자가 필요할 때만 사용
     public static final Long DAILY_CHECKLIST_ID = 1L;
     public static final Long TASK_ID = 2L;
     public static final Long SUB_TASK_ID = 3L;
 
+
+    // 생성 메서드에서 사용됨. 테스트 중간에 사용되면 이미 존재하는 Task나 SubTask임
     public static final String TASK_NAME = "taskName";
     public static final String TASK_NAME2 = "taskName2";
     public static final String TASK_NAME3 = "taskName3";
@@ -28,12 +31,13 @@ public class ChecklistDataHelper {
     public static final String SUB_TASK_NAME = "subTaskName";
     public static final String SUB_TASK_NAME2 = "subTaskName2";
 
-    // given절 중복 메서드를 제외하고, Task나 SubTask를 생성해야 할 때만 사용
+
+    // 생성 메서드를 제외하고, Task나 SubTask를 생성해야 할 때만 사용
     public static final String NEW_TASK_NAME = "newTaskName";
     public static final String NEW_SUB_TASK_NAME = "newSubTaskName";
 
 
-    // checklist request dto
+    // checklist Task request dto
     public static TaskSaveRequest createTaskSaveRequest(Long dailyChecklistId, String taskName) {
         return new TaskSaveRequest(dailyChecklistId, taskName);
     }
@@ -50,6 +54,8 @@ public class ChecklistDataHelper {
         return new TaskDeleteRequest(dailyChecklistId);
     }
 
+
+    // checklist SubTask request dto
     public static SubTaskSaveRequest createSubTaskSaveRequest(Long dailyChecklistId, String taskName, String subTaskName) {
         return new SubTaskSaveRequest(dailyChecklistId, taskName, subTaskName);
     }
@@ -63,7 +69,7 @@ public class ChecklistDataHelper {
     }
 
 
-    // checklist response dto
+    // checklist Task response dto
     public static TaskSaveResponse createTaskSaveResponse(Long taskId, String taskName) {
         return new TaskSaveResponse(taskId, taskName);
     }
@@ -76,6 +82,8 @@ public class ChecklistDataHelper {
         return new TaskDifficultyModifyResponse(taskName, modifiedDifficulty);
     }
 
+
+    // checklist SubTask response dto
     public static SubTaskSaveResponse createSubTaskSaveResponse(Long subTaskId, String subtaskName) {
         return new SubTaskSaveResponse(subTaskId, subtaskName);
     }
