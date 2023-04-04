bfe5dd0299bf23718d1de12af6600e6324a4cdf3
hwicode <khc3796@naver.com>
2023-04-04 10:42:42 +0900

    Refactor: DailyChecklistFindService의 생성자를 private로 수정  -#28

--- _schedule/schedule/src/main/java/hwicode/schedule/dailyschedule/checklist/application/DailyChecklistFindService.java
+++ _schedule/schedule/src/main/java/hwicode/schedule/dailyschedule/checklist/application/DailyChecklistFindService.java
@@ -3,7 +3,10 @@ package hwicode.schedule.dailyschedule.checklist.application;
 import hwicode.schedule.dailyschedule.checklist.domain.DailyChecklist;
 import hwicode.schedule.dailyschedule.checklist.exception.dailyckecklist_find_service.DailyChecklistNotFoundException;
 import hwicode.schedule.dailyschedule.checklist.infra.DailyChecklistRepository;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DailyChecklistFindService {
 
     static DailyChecklist findDailyChecklistWithTasks(DailyChecklistRepository dailyChecklistRepository, Long dailyChecklistId) {
