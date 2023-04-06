aa87cf8a21aa1d2ede4a4e53da982cd29c1a3259
hwicode <khc3796@naver.com>
2023-04-06 14:41:54 +0900

    Feature: DatabaseCleanUp에서 테이블의 이름을 가져오는 로직 변경 -#29
    - 기존에는 테이블의 이름을 가져올 때 @Entity가 붙은 클래스의 이름을 가져왔다.
    - 그런데 이제는 @Entity가 붙어있어도 테이블의 이름과 클래스의 이름이 동일하지 않다.
    - 그래서 @Table이 붙는 경우를 로직에 추가해줬다. -> 이름의 중복값이 있으므로 Set을 활용함

--- _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/checklist/DatabaseCleanUp.java
+++ _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/checklist/DatabaseCleanUp.java
@@ -8,7 +8,10 @@ import javax.annotation.PostConstruct;
 import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Table;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -17,14 +20,25 @@ public class DatabaseCleanUp {
     @PersistenceContext
     private EntityManager entityManager;
 
-    private List<String> tableNames;
+    private final List<String> tableNames = new ArrayList<>();
 
     @PostConstruct
     public void init() {
-        tableNames = entityManager.getMetamodel().getEntities().stream()
+        List<String> entityNames = entityManager.getMetamodel().getEntities().stream()
                 .filter(e -> e.getJavaType().getAnnotation(Entity.class) != null)
+                .filter(e -> e.getJavaType().getAnnotation(Table.class) == null)
                 .map(e -> CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, e.getName()))
                 .collect(Collectors.toList());
+
+        Set<String> tableAnnotationNames = entityManager.getMetamodel().getEntities().stream()
+                .filter(e -> e.getJavaType().getAnnotation(Table.class) != null)
+                .map(e -> CaseFormat.UPPER_CAMEL.to(
+                        CaseFormat.LOWER_UNDERSCORE, e.getJavaType().getAnnotation(Table.class).name()
+                ))
+                .collect(Collectors.toSet());
+
+        tableNames.addAll(entityNames);
+        tableNames.addAll(tableAnnotationNames);
     }
 
     @Transactional
