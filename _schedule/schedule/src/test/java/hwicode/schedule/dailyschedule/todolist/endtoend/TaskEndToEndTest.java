f49e0aeaa69c52e77e60123b3aaf46c47dd43111
hwicode <khc3796@naver.com>
2023-04-13 12:03:16 +0900

    Test: TaskEndToEndTest 생성 -#44
    - TaskController에 요청을 보냄

---
+++ _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/todolist/endtoend/TaskEndToEndTest.java
@@ -0,0 +1,166 @@
+package hwicode.schedule.dailyschedule.todolist.endtoend;
+
+import hwicode.schedule.DatabaseCleanUp;
+import hwicode.schedule.dailyschedule.todolist.application.TaskSaveAndDeleteService;
+import hwicode.schedule.dailyschedule.todolist.domain.DailyToDoList;
+import hwicode.schedule.dailyschedule.todolist.domain.Importance;
+import hwicode.schedule.dailyschedule.todolist.domain.Priority;
+import hwicode.schedule.dailyschedule.todolist.domain.Task;
+import hwicode.schedule.dailyschedule.todolist.infra.DailyToDoListRepository;
+import hwicode.schedule.dailyschedule.todolist.infra.TaskRepository;
+import hwicode.schedule.dailyschedule.todolist.presentation.task.dto.delete.TaskDeleteRequest;
+import hwicode.schedule.dailyschedule.todolist.presentation.task.dto.information_modify.TaskInformationModifyRequest;
+import hwicode.schedule.dailyschedule.todolist.presentation.task.dto.name_modify.TaskNameModifyRequest;
+import hwicode.schedule.dailyschedule.todolist.presentation.task.dto.save.TaskSaveRequest;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+import static hwicode.schedule.dailyschedule.todolist.ToDoListDataHelper.*;
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class TaskEndToEndTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    DatabaseCleanUp databaseCleanUp;
+
+    @Autowired
+    DailyToDoListRepository dailyToDoListRepository;
+
+    @Autowired
+    TaskSaveAndDeleteService taskSaveAndDeleteService;
+
+    @Autowired
+    TaskRepository taskRepository;
+
+    @BeforeEach
+    void clearDatabase() {
+        databaseCleanUp.execute();
+    }
+
+    @Test
+    void 과제_생성_요청() {
+        //given
+        DailyToDoList dailyToDoList = new DailyToDoList();
+        dailyToDoListRepository.save(dailyToDoList);
+
+        TaskSaveRequest taskSaveRequest = createTaskSaveRequest(dailyToDoList.getId(), TASK_NAME);
+
+        RequestSpecification requestSpecification = given()
+                .contentType(ContentType.JSON)
+                .body(taskSaveRequest);
+
+        //when
+        Response response = requestSpecification.when()
+                .post(String.format("http://localhost:%s/dailyschedule/todolist/tasks", port));
+
+        //then
+        response.then()
+                .statusCode(HttpStatus.CREATED.value());
+
+        List<Task> all = taskRepository.findAll();
+        assertThat(all.size()).isEqualTo(1);
+    }
+
+    @Test
+    void 과제_삭제_요청() {
+        //given
+        DailyToDoList dailyToDoList = new DailyToDoList();
+        dailyToDoListRepository.save(dailyToDoList);
+
+        Long taskId = taskSaveAndDeleteService.save(
+                createTaskSaveRequest(DAILY_TO_DO_LIST_ID, TASK_NAME)
+        );
+
+        TaskDeleteRequest taskDeleteRequest = createTaskDeleteRequest(dailyToDoList.getId());
+
+        RequestSpecification requestSpecification = given()
+                .pathParam("taskName", TASK_NAME)
+                .contentType(ContentType.JSON)
+                .body(taskDeleteRequest);
+
+        //when
+        Response response = requestSpecification.when()
+                .delete(String.format("http://localhost:%s/dailyschedule/todolist/tasks/{taskName}", port));
+
+        //then
+        response.then()
+                .statusCode(HttpStatus.NO_CONTENT.value());
+
+        assertThat(taskRepository.existsById(taskId)).isFalse();
+    }
+
+    @Test
+    void 과제_정보_변경_요청() {
+        //given
+        DailyToDoList dailyToDoList = new DailyToDoList();
+        dailyToDoListRepository.save(dailyToDoList);
+
+        Long taskId = taskSaveAndDeleteService.save(
+                createTaskSaveRequest(DAILY_TO_DO_LIST_ID, TASK_NAME)
+        );
+
+        TaskInformationModifyRequest taskInformationModifyRequest = createTaskInformationModifyRequest(Priority.THIRD, Importance.THIRD);
+
+        RequestSpecification requestSpecification = given()
+                .pathParam("taskId", taskId)
+                .contentType(ContentType.JSON)
+                .body(taskInformationModifyRequest);
+
+        //when
+        Response response = requestSpecification.when()
+                .patch(String.format("http://localhost:%s/dailyschedule/todolist/tasks/{taskId}/information", port));
+
+        //then
+        response.then()
+                .statusCode(HttpStatus.OK.value());
+
+        Task task = taskRepository.findById(taskId).orElseThrow();
+        assertThat(task.changePriority(Priority.THIRD)).isFalse();
+        assertThat(task.changeImportance(Importance.THIRD)).isFalse();
+    }
+
+    @Test
+    void 과제_이름_변경_요청() {
+        //given
+        DailyToDoList dailyToDoList = new DailyToDoList();
+        dailyToDoListRepository.save(dailyToDoList);
+
+        Long taskId = taskSaveAndDeleteService.save(
+                createTaskSaveRequest(DAILY_TO_DO_LIST_ID, TASK_NAME)
+        );
+
+        TaskNameModifyRequest taskNameModifyRequest = createTaskNameModifyRequest(dailyToDoList.getId(), NEW_TASK_NAME);
+
+        RequestSpecification requestSpecification = given()
+                .pathParam("taskName", TASK_NAME)
+                .contentType(ContentType.JSON)
+                .body(taskNameModifyRequest);
+
+        //when
+        Response response = requestSpecification.when()
+                .patch(String.format("http://localhost:%s/dailyschedule/todolist/tasks/{taskName}", port));
+
+        //then
+        response.then()
+                .statusCode(HttpStatus.OK.value());
+
+        Task task = taskRepository.findById(taskId).orElseThrow();
+        assertThat(task.getName()).isEqualTo(NEW_TASK_NAME);
+    }
+}
