b1b9a7a46cbb434f722540e1afae1ed7269bb758
hwicode <khc3796@naver.com>
2023-04-18 07:45:20 +0900

    Test: 과체 체커의 이름 변경 기능 엔드 투 엔드 테스트 작성 -#46

--- _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/checklist/endtoend/TaskCheckerEndToEndTest.java
+++ _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/checklist/endtoend/TaskCheckerEndToEndTest.java
@@ -4,9 +4,12 @@ import hwicode.schedule.DatabaseCleanUp;
 import hwicode.schedule.dailyschedule.checklist.application.TaskCheckerService;
 import hwicode.schedule.dailyschedule.checklist.domain.DailyChecklist;
 import hwicode.schedule.dailyschedule.checklist.domain.TaskChecker;
+import hwicode.schedule.dailyschedule.checklist.exception.domain.dailychecklist.TaskCheckerNameDuplicationException;
 import hwicode.schedule.dailyschedule.checklist.infra.DailyChecklistRepository;
 import hwicode.schedule.dailyschedule.checklist.infra.TaskCheckerRepository;
 import hwicode.schedule.dailyschedule.checklist.presentation.task_checker.dto.difficulty_modify.TaskDifficultyModifyRequest;
+import hwicode.schedule.dailyschedule.checklist.presentation.task_checker.dto.name_modify.TaskCheckerNameModifyRequest;
+import hwicode.schedule.dailyschedule.checklist.presentation.task_checker.dto.save.TaskCheckerSaveRequest;
 import hwicode.schedule.dailyschedule.checklist.presentation.task_checker.dto.status_modify.TaskStatusModifyRequest;
 import hwicode.schedule.dailyschedule.dailyschedule_domain.Difficulty;
 import hwicode.schedule.dailyschedule.checklist.domain.TaskStatus;
@@ -23,6 +26,7 @@ import org.springframework.http.HttpStatus;
 
 import static hwicode.schedule.dailyschedule.checklist.ChecklistDataHelper.*;
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
@@ -106,4 +110,35 @@ class TaskCheckerEndToEndTest {
         TaskChecker taskChecker = taskCheckerRepository.findById(taskCheckerId).orElseThrow();
         assertThat(taskChecker.getDifficultyScore()).isEqualTo(Difficulty.HARD.getValue());
     }
+
+    @Test
+    void 과제체커_이름_변경_요청() {
+        // given
+        DailyChecklist dailyChecklist = new DailyChecklist();
+        dailyChecklistRepository.save(dailyChecklist);
+
+        taskCheckerService.saveTaskChecker(
+                createTaskCheckerSaveRequest(dailyChecklist.getId(), TASK_CHECKER_NAME, Difficulty.NORMAL)
+        );
+
+        TaskCheckerNameModifyRequest taskCheckerNameModifyRequest = createTaskCheckerNameModifyRequest(dailyChecklist.getId(), NEW_TASK_CHECKER_NAME);
+
+        RequestSpecification requestSpecification = given()
+                .pathParam("taskCheckerName", TASK_CHECKER_NAME)
+                .contentType(ContentType.JSON)
+                .body(taskCheckerNameModifyRequest);
+
+        // when
+        Response response = requestSpecification.when()
+                .patch(String.format("http://localhost:%s/dailyschedule/checklist/taskCheckers/{taskCheckerName}/name", port));
+
+        // then
+        response.then()
+                .statusCode(HttpStatus.OK.value());
+
+        TaskCheckerSaveRequest taskCheckerSaveRequest = createTaskCheckerSaveRequest(dailyChecklist.getId(), NEW_TASK_CHECKER_NAME, Difficulty.NORMAL);
+        assertThatThrownBy(() -> taskCheckerService.saveTaskChecker(taskCheckerSaveRequest))
+                .isInstanceOf(TaskCheckerNameDuplicationException.class);
+    }
+
 }
