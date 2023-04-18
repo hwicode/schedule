eca7086bae62c51929c6b9b7c20ee046146fbea1
hwicode <khc3796@naver.com>
2023-04-18 10:26:54 +0900

    Test: DailyToDoList 컨트롤러에 정보를 변경하는 기능 엔드 투 엔드 테스트 작성 -#49

---
+++ _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/todolist/endtoend/DailyToDoListEndToEndTest.java
@@ -0,0 +1,73 @@
+package hwicode.schedule.dailyschedule.todolist.endtoend;
+
+import hwicode.schedule.DatabaseCleanUp;
+import hwicode.schedule.dailyschedule.todolist.application.TaskSaveAndDeleteService;
+import hwicode.schedule.dailyschedule.todolist.domain.*;
+import hwicode.schedule.dailyschedule.todolist.infra.DailyToDoListRepository;
+import hwicode.schedule.dailyschedule.todolist.infra.TaskRepository;
+import hwicode.schedule.dailyschedule.todolist.presentation.dailytodolist.dto.DailyToDoListInformationChangeRequest;
+import hwicode.schedule.dailyschedule.todolist.presentation.task.dto.delete.TaskDeleteRequest;
+import hwicode.schedule.dailyschedule.todolist.presentation.task.dto.information_modify.TaskInformationModifyRequest;
+import hwicode.schedule.dailyschedule.todolist.presentation.task.dto.save.TaskSaveRequest;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+import static hwicode.schedule.dailyschedule.todolist.ToDoListDataHelper.*;
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class DailyToDoListEndToEndTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    DatabaseCleanUp databaseCleanUp;
+
+    @Autowired
+    DailyToDoListRepository dailyToDoListRepository;
+
+    @BeforeEach
+    void clearDatabase() {
+        databaseCleanUp.execute();
+    }
+
+    @Test
+    void 투두리스트_정보_변경_요청() {
+        //given
+        DailyToDoList dailyToDoList = new DailyToDoList(Emoji.NOT_BAD);
+        dailyToDoListRepository.save(dailyToDoList);
+
+        String review = "좋은데!";
+        DailyToDoListInformationChangeRequest dailyToDoListInformationChangeRequest = createDailyToDoListInformationChangeRequest(review, Emoji.GOOD);
+
+        RequestSpecification requestSpecification = given()
+                .pathParam("dailyToDoListId", dailyToDoList.getId())
+                .contentType(ContentType.JSON)
+                .body(dailyToDoListInformationChangeRequest);
+
+        //when
+        Response response = requestSpecification.when()
+                .patch(String.format("http://localhost:%s/dailyschedule/todolist/dailytodolists/{dailyToDoListId}/information", port));
+
+        //then
+        response.then()
+                .statusCode(HttpStatus.OK.value());
+
+        DailyToDoList savedDailyToDoList = dailyToDoListRepository.findById(dailyToDoList.getId()).orElseThrow();
+        assertThat(savedDailyToDoList.writeReview(review)).isFalse();
+        assertThat(savedDailyToDoList.changeTodayEmoji(Emoji.GOOD)).isFalse();
+    }
+}
