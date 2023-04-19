22ad8f5a3404eb79ed88e59327c436c5a7674044
hwicode <khc3796@naver.com>
2023-04-19 09:00:58 +0900

    Feature: LearningTime 클래스의 학습 주제가 SubTask일 때 삭제하는 기능 구현 -#50
    - 학습 주제가 SubTask인 경우 구현함

--- _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/timetable/domain/LearningTimeTest.java
+++ _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/timetable/domain/LearningTimeTest.java
@@ -32,12 +32,25 @@ class LearningTimeTest {
         assertThat(learningTime.deleteSubject()).isFalse();
     }
 
+    @Test
+    void 학습_시간에서_SubTask_학습_주제를_삭제할_수_있다() {
+        // given
+        LearningTime learningTime = new LearningTime(new SubjectOfSubTask(subject));
+
+        // when
+        learningTime.deleteSubject();
+
+        // then
+        assertThat(learningTime.deleteSubject()).isFalse();
+    }
+
 }
 
 class LearningTime {
 
     private String subject;
     private SubjectOfTask subjectOfTask;
+    private SubjectOfSubTask subjectOfSubTask;
 
     LearningTime(String subject) {
         this.subject = subject;
@@ -47,12 +60,17 @@ class LearningTime {
         this.subjectOfTask = subjectOfTask;
     }
 
+    LearningTime(SubjectOfSubTask subjectOfSubTask) {
+        this.subjectOfSubTask = subjectOfSubTask;
+    }
+
     public boolean deleteSubject() {
-        if (this.subject == null && subjectOfTask == null) {
+        if (this.subject == null && subjectOfTask == null && subjectOfSubTask == null) {
             return false;
         }
         this.subject = null;
         this.subjectOfTask = null;
+        this.subjectOfSubTask = null;
         return true;
     }
 }
@@ -62,6 +80,15 @@ class SubjectOfTask {
     private String name;
 
     SubjectOfTask(String name) {
+        this.name = name;
+    }
+}
+
+class SubjectOfSubTask {
+
+    private String name;
+
+    SubjectOfSubTask(String name) {
         this.name = name;
     }
 }
