91a33a8c411001e305615cb70252f91e953c5128
hwicode <khc3796@naver.com>
2023-04-21 08:57:34 +0900

    Feature: TimeTableService 클래스에서 특정 학습 주제의 총 학습 시간을 계산하는 기능 구현 -#54
    - 특정 학습 주제의 총 학습 시간을 계산하는 기능 통합 테스트 작성

--- _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/timetable/application/TimeTableServiceIntegrationTest.java
+++ _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/timetable/application/TimeTableServiceIntegrationTest.java
@@ -1,10 +1,10 @@
 package hwicode.schedule.dailyschedule.timetable.application;
 
 import hwicode.schedule.DatabaseCleanUp;
-import hwicode.schedule.dailyschedule.timetable.domain.LearningTime;
-import hwicode.schedule.dailyschedule.timetable.domain.LearningTimeSaveOnlyRepository;
-import hwicode.schedule.dailyschedule.timetable.domain.TimeTable;
+import hwicode.schedule.dailyschedule.timetable.domain.*;
 import hwicode.schedule.dailyschedule.timetable.infra.LearningTimeRepository;
+import hwicode.schedule.dailyschedule.timetable.infra.SubjectOfSubTaskRepository;
+import hwicode.schedule.dailyschedule.timetable.infra.SubjectOfTaskRepository;
 import hwicode.schedule.dailyschedule.timetable.infra.TimeTableRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -32,6 +32,12 @@ class TimeTableServiceIntegrationTest {
 
     @Autowired
     LearningTimeRepository learningTimeRepository;
+
+    @Autowired
+    SubjectOfTaskRepository subjectOfTaskRepository;
+
+    @Autowired
+    SubjectOfSubTaskRepository subjectOfSubTaskRepository;
 
     @BeforeEach
     void clearDatabase() {
@@ -109,6 +115,70 @@ class TimeTableServiceIntegrationTest {
         assertThat(savedTimeTable.getTotalLearningTime()).isZero();
     }
 
+    @Test
+    void 타임_테이블에_존재하는_특정_학습_주제의_총_학습_시간을_계산할_수_있다() {
+        // given
+        LocalDateTime startTime = LocalDateTime.of(2023, 4, 20, 0, 0);
+        LocalDateTime endTime = startTime.plusMinutes(30);
+
+        TimeTable timeTable = new TimeTable(startTime.toLocalDate());
+        LearningTime learningTime = timeTable.createLearningTime(startTime);
+        timeTable.changeLearningTimeEndTime(startTime, endTime);
+        learningTime.changeSubject("subject");
+
+        timeTableRepository.save(timeTable);
+
+        // when
+        int totalLearningTime = timeTableService.calculateSubjectTotalLearningTime(timeTable.getId(), "subject");
+
+        // then
+        assertThat(totalLearningTime).isEqualTo(30);
+    }
+
+    @Test
+    void 타임_테이블에_존재하는_특정_Task_학습_주제의_총_학습_시간을_계산할_수_있다() {
+        // given
+        LocalDateTime startTime = LocalDateTime.of(2023, 4, 20, 0, 0);
+        LocalDateTime endTime = startTime.plusMinutes(30);
+
+        SubjectOfTask subjectOfTask = subjectOfTaskRepository.save(new SubjectOfTask("학습 주제"));
+
+        TimeTable timeTable = new TimeTable(startTime.toLocalDate());
+        LearningTime learningTime = timeTable.createLearningTime(startTime);
+        timeTable.changeLearningTimeEndTime(startTime, endTime);
+        learningTime.changeSubjectOfTask(subjectOfTask);
+
+        timeTableRepository.save(timeTable);
+
+        // when
+        int totalLearningTime = timeTableService.calculateSubjectOfTaskTotalLearningTime(timeTable.getId(), subjectOfTask.getId());
+
+        // then
+        assertThat(totalLearningTime).isEqualTo(30);
+    }
+
+    @Test
+    void 타임_테이블에_존재하는_특정_SubTask_학습_주제의_총_학습_시간을_계산할_수_있다() {
+        // given
+        LocalDateTime startTime = LocalDateTime.of(2023, 4, 20, 0, 0);
+        LocalDateTime endTime = startTime.plusMinutes(30);
+
+        SubjectOfSubTask subjectOfSubTask = subjectOfSubTaskRepository.save(new SubjectOfSubTask("학습 주제"));
+
+        TimeTable timeTable = new TimeTable(startTime.toLocalDate());
+        LearningTime learningTime = timeTable.createLearningTime(startTime);
+        timeTable.changeLearningTimeEndTime(startTime, endTime);
+        learningTime.changeSubjectOfSubTask(subjectOfSubTask);
+
+        timeTableRepository.save(timeTable);
+
+        // when
+        int totalLearningTime = timeTableService.calculateSubjectOfSubTaskTotalLearningTime(timeTable.getId(), subjectOfSubTask.getId());
+
+        // then
+        assertThat(totalLearningTime).isEqualTo(30);
+    }
+
 }
 
 @Service
@@ -117,8 +187,13 @@ class TimeTableService {
     private final TimeTableRepository timeTableRepository;
     private final LearningTimeSaveOnlyRepository learningTimeSaveOnlyRepository;
 
-    TimeTableService(TimeTableRepository timeTableRepository, LearningTimeSaveOnlyRepository learningTimeSaveOnlyRepository) {
+    private final SubjectOfTaskRepository subjectOfTaskRepository;
+    private final SubjectOfSubTaskRepository subjectOfSubTaskRepository;
+
+    TimeTableService(TimeTableRepository timeTableRepository, SubjectOfTaskRepository subjectOfTaskRepository, SubjectOfSubTaskRepository subjectOfSubTaskRepository, LearningTimeSaveOnlyRepository learningTimeSaveOnlyRepository) {
         this.timeTableRepository = timeTableRepository;
+        this.subjectOfTaskRepository = subjectOfTaskRepository;
+        this.subjectOfSubTaskRepository = subjectOfSubTaskRepository;
         this.learningTimeSaveOnlyRepository = learningTimeSaveOnlyRepository;
     }
 
@@ -149,6 +224,30 @@ class TimeTableService {
     public void deleteLearningTime(Long timeTableId, LocalDateTime startTime) {
         TimeTable timeTable = findTimeTableById(timeTableId);
         timeTable.deleteLearningTime(startTime);
+    }
+
+    @Transactional
+    public int calculateSubjectTotalLearningTime(Long timeTableId, String subject) {
+        TimeTable timeTable = findTimeTableById(timeTableId);
+        return timeTable.getSubjectTotalLearningTime(subject);
+    }
+
+    @Transactional
+    public int calculateSubjectOfTaskTotalLearningTime(Long timeTableId, Long subjectOfTaskId) {
+        TimeTable timeTable = findTimeTableById(timeTableId);
+        SubjectOfTask subjectOfTask = subjectOfTaskRepository.findById(subjectOfTaskId)
+                .orElseThrow(IllegalArgumentException::new);
+
+        return timeTable.getSubjectOfTaskTotalLearningTime(subjectOfTask);
+    }
+
+    @Transactional
+    public int calculateSubjectOfSubTaskTotalLearningTime(Long timeTableId, Long subjectOfSubTaskId) {
+        TimeTable timeTable = findTimeTableById(timeTableId);
+        SubjectOfSubTask subjectOfSubTask = subjectOfSubTaskRepository.findById(subjectOfSubTaskId)
+                .orElseThrow(IllegalArgumentException::new);
+
+        return timeTable.getSubjectOfSubTaskTotalLearningTime(subjectOfSubTask);
     }
 
     private TimeTable findTimeTableById(Long timeTableId) {
