fce9f4f7a76a7649a47af7aef2712a6c0e63452e
hwicode <khc3796@naver.com>
2023-04-21 09:00:35 +0900

    Refactor: TimeTableService 클래스를 application 패키지로 옮김 -#54

---
+++ _schedule/schedule/src/main/java/hwicode/schedule/dailyschedule/timetable/application/TimeTableService.java
@@ -0,0 +1,82 @@
+package hwicode.schedule.dailyschedule.timetable.application;
+
+import hwicode.schedule.dailyschedule.timetable.domain.*;
+import hwicode.schedule.dailyschedule.timetable.infra.SubjectOfSubTaskRepository;
+import hwicode.schedule.dailyschedule.timetable.infra.SubjectOfTaskRepository;
+import hwicode.schedule.dailyschedule.timetable.infra.TimeTableRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+class TimeTableService {
+
+    private final TimeTableRepository timeTableRepository;
+    private final LearningTimeSaveOnlyRepository learningTimeSaveOnlyRepository;
+
+    private final SubjectOfTaskRepository subjectOfTaskRepository;
+    private final SubjectOfSubTaskRepository subjectOfSubTaskRepository;
+
+    @Transactional
+    public Long createLearningTime(Long timeTableId, LocalDateTime startTime) {
+        TimeTable timeTable = findTimeTableById(timeTableId);
+
+        LearningTime learningTime = timeTable.createLearningTime(startTime);
+        return learningTimeSaveOnlyRepository.save(learningTime)
+                .getId();
+    }
+
+    @Transactional
+    public LocalDateTime changeLearningTimeStartTime(Long timeTableId, LocalDateTime startTime, LocalDateTime newStartTime) {
+        TimeTable timeTable = findTimeTableById(timeTableId);
+
+        return timeTable.changeLearningTimeStartTime(startTime, newStartTime);
+    }
+
+    @Transactional
+    public LocalDateTime changeLearningTimeEndTime(Long timeTableId, LocalDateTime startTime, LocalDateTime endTime) {
+        TimeTable timeTable = findTimeTableById(timeTableId);
+
+        return timeTable.changeLearningTimeEndTime(startTime, endTime);
+    }
+
+    @Transactional
+    public void deleteLearningTime(Long timeTableId, LocalDateTime startTime) {
+        TimeTable timeTable = findTimeTableById(timeTableId);
+        timeTable.deleteLearningTime(startTime);
+    }
+
+    @Transactional
+    public int calculateSubjectTotalLearningTime(Long timeTableId, String subject) {
+        TimeTable timeTable = findTimeTableById(timeTableId);
+        return timeTable.getSubjectTotalLearningTime(subject);
+    }
+
+    @Transactional
+    public int calculateSubjectOfTaskTotalLearningTime(Long timeTableId, Long subjectOfTaskId) {
+        TimeTable timeTable = findTimeTableById(timeTableId);
+        SubjectOfTask subjectOfTask = subjectOfTaskRepository.findById(subjectOfTaskId)
+                .orElseThrow(IllegalArgumentException::new);
+
+        return timeTable.getSubjectOfTaskTotalLearningTime(subjectOfTask);
+    }
+
+    @Transactional
+    public int calculateSubjectOfSubTaskTotalLearningTime(Long timeTableId, Long subjectOfSubTaskId) {
+        TimeTable timeTable = findTimeTableById(timeTableId);
+        SubjectOfSubTask subjectOfSubTask = subjectOfSubTaskRepository.findById(subjectOfSubTaskId)
+                .orElseThrow(IllegalArgumentException::new);
+
+        return timeTable.getSubjectOfSubTaskTotalLearningTime(subjectOfSubTask);
+    }
+
+
+    private TimeTable findTimeTableById(Long timeTableId) {
+        return timeTableRepository.findTimeTableWithLearningTimes(timeTableId)
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+}
--- _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/timetable/application/TimeTableServiceIntegrationTest.java
+++ _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/timetable/application/TimeTableServiceIntegrationTest.java
@@ -1,7 +1,10 @@
 package hwicode.schedule.dailyschedule.timetable.application;
 
 import hwicode.schedule.DatabaseCleanUp;
-import hwicode.schedule.dailyschedule.timetable.domain.*;
+import hwicode.schedule.dailyschedule.timetable.domain.LearningTime;
+import hwicode.schedule.dailyschedule.timetable.domain.SubjectOfSubTask;
+import hwicode.schedule.dailyschedule.timetable.domain.SubjectOfTask;
+import hwicode.schedule.dailyschedule.timetable.domain.TimeTable;
 import hwicode.schedule.dailyschedule.timetable.infra.LearningTimeRepository;
 import hwicode.schedule.dailyschedule.timetable.infra.SubjectOfSubTaskRepository;
 import hwicode.schedule.dailyschedule.timetable.infra.SubjectOfTaskRepository;
@@ -10,8 +13,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
@@ -177,82 +178,6 @@ class TimeTableServiceIntegrationTest {
 
         // then
         assertThat(totalLearningTime).isEqualTo(30);
-    }
-
-}
-
-@Service
-class TimeTableService {
-
-    private final TimeTableRepository timeTableRepository;
-    private final LearningTimeSaveOnlyRepository learningTimeSaveOnlyRepository;
-
-    private final SubjectOfTaskRepository subjectOfTaskRepository;
-    private final SubjectOfSubTaskRepository subjectOfSubTaskRepository;
-
-    TimeTableService(TimeTableRepository timeTableRepository, SubjectOfTaskRepository subjectOfTaskRepository, SubjectOfSubTaskRepository subjectOfSubTaskRepository, LearningTimeSaveOnlyRepository learningTimeSaveOnlyRepository) {
-        this.timeTableRepository = timeTableRepository;
-        this.subjectOfTaskRepository = subjectOfTaskRepository;
-        this.subjectOfSubTaskRepository = subjectOfSubTaskRepository;
-        this.learningTimeSaveOnlyRepository = learningTimeSaveOnlyRepository;
-    }
-
-    @Transactional
-    public Long createLearningTime(Long timeTableId, LocalDateTime startTime) {
-        TimeTable timeTable = findTimeTableById(timeTableId);
-
-        LearningTime learningTime = timeTable.createLearningTime(startTime);
-        return learningTimeSaveOnlyRepository.save(learningTime)
-                .getId();
-    }
-
-    @Transactional
-    public LocalDateTime changeLearningTimeStartTime(Long timeTableId, LocalDateTime startTime, LocalDateTime newStartTime) {
-        TimeTable timeTable = findTimeTableById(timeTableId);
-
-        return timeTable.changeLearningTimeStartTime(startTime, newStartTime);
-    }
-
-    @Transactional
-    public LocalDateTime changeLearningTimeEndTime(Long timeTableId, LocalDateTime startTime, LocalDateTime endTime) {
-        TimeTable timeTable = findTimeTableById(timeTableId);
-
-        return timeTable.changeLearningTimeEndTime(startTime, endTime);
-    }
-
-    @Transactional
-    public void deleteLearningTime(Long timeTableId, LocalDateTime startTime) {
-        TimeTable timeTable = findTimeTableById(timeTableId);
-        timeTable.deleteLearningTime(startTime);
-    }
-
-    @Transactional
-    public int calculateSubjectTotalLearningTime(Long timeTableId, String subject) {
-        TimeTable timeTable = findTimeTableById(timeTableId);
-        return timeTable.getSubjectTotalLearningTime(subject);
-    }
-
-    @Transactional
-    public int calculateSubjectOfTaskTotalLearningTime(Long timeTableId, Long subjectOfTaskId) {
-        TimeTable timeTable = findTimeTableById(timeTableId);
-        SubjectOfTask subjectOfTask = subjectOfTaskRepository.findById(subjectOfTaskId)
-                .orElseThrow(IllegalArgumentException::new);
-
-        return timeTable.getSubjectOfTaskTotalLearningTime(subjectOfTask);
-    }
-
-    @Transactional
-    public int calculateSubjectOfSubTaskTotalLearningTime(Long timeTableId, Long subjectOfSubTaskId) {
-        TimeTable timeTable = findTimeTableById(timeTableId);
-        SubjectOfSubTask subjectOfSubTask = subjectOfSubTaskRepository.findById(subjectOfSubTaskId)
-                .orElseThrow(IllegalArgumentException::new);
-
-        return timeTable.getSubjectOfSubTaskTotalLearningTime(subjectOfSubTask);
-    }
-
-    private TimeTable findTimeTableById(Long timeTableId) {
-        return timeTableRepository.findTimeTableWithLearningTimes(timeTableId)
-                .orElseThrow(IllegalArgumentException::new);
     }
 
 }
