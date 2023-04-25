2e8afa5f21dbd1526fe243950fbb689be4b83420
hwicode <khc3796@naver.com>
2023-04-25 11:49:31 +0900

    Test: 학습 시간 끝나는 시간 변경 요청 엔드 투 엔드 테스트 작성함 -#59

--- _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/timetable/endtoend/TimeTableEndToEndTest.java
+++ _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/timetable/endtoend/TimeTableEndToEndTest.java
@@ -5,6 +5,7 @@ import hwicode.schedule.dailyschedule.timetable.application.TimeTableService;
 import hwicode.schedule.dailyschedule.timetable.domain.TimeTable;
 import hwicode.schedule.dailyschedule.timetable.exception.domain.timetablevalidator.StartTimeDuplicateException;
 import hwicode.schedule.dailyschedule.timetable.infra.TimeTableRepository;
+import hwicode.schedule.dailyschedule.timetable.presentation.timetable.dto.endtime_modify.EndTimeModifyRequest;
 import hwicode.schedule.dailyschedule.timetable.presentation.timetable.dto.save.LearningTimeSaveRequest;
 import hwicode.schedule.dailyschedule.timetable.presentation.timetable.dto.starttime_modify.StartTimeModifyRequest;
 import io.restassured.http.ContentType;
@@ -17,8 +18,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 
+import java.time.LocalDateTime;
+
 import static hwicode.schedule.dailyschedule.timetable.TimeTableDataHelper.*;
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -90,4 +94,31 @@ class TimeTableEndToEndTest {
         assertThatThrownBy(() -> timeTableService.saveLearningTime(timeTable.getId(), NEW_START_TIME))
                 .isInstanceOf(StartTimeDuplicateException.class);
     }
+
+    @Test
+    void 학습_시간_끝나는_시간_변경_요청() {
+        // given
+        TimeTable timeTable = new TimeTable(START_TIME.toLocalDate());
+        timeTable.createLearningTime(START_TIME);
+        timeTableRepository.save(timeTable);
+
+        LocalDateTime endTime = START_TIME.plusMinutes(30);
+        EndTimeModifyRequest endTimeModifyRequest = createEndTimeModifyRequest(timeTable.getId(), endTime);
+
+        RequestSpecification requestSpecification = given()
+                .contentType(ContentType.JSON)
+                .body(endTimeModifyRequest);
+
+        // when
+        Response response = requestSpecification.when()
+                .patch(String.format("http://localhost:%s/dailyschedule/timetable/%s/endtime", port, START_TIME));
+
+        // then
+        response.then()
+                .statusCode(HttpStatus.OK.value());
+
+        TimeTable savedTimeTable = timeTableRepository.findTimeTableWithLearningTimes(timeTable.getId()).orElseThrow();
+        assertThat(savedTimeTable.getTotalLearningTime()).isEqualTo(30);
+    }
+
 }
