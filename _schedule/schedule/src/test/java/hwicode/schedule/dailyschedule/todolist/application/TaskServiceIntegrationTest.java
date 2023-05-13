7e32e6a33b864f8bc327ad67563ef3278d37afdd
hwicode <khc3796@naver.com>
2023-05-13 16:07:04 +0900

    Test: TaskServiceIntegrationTest에 과제가 존재하지 않는 경우 테스트코드 추가 -#43

--- _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/todolist/application/TaskServiceIntegrationTest.java
+++ _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/todolist/application/TaskServiceIntegrationTest.java
@@ -2,6 +2,7 @@ package hwicode.schedule.dailyschedule.todolist.application;
 
 import hwicode.schedule.DatabaseCleanUp;
 import hwicode.schedule.dailyschedule.todolist.domain.*;
+import hwicode.schedule.dailyschedule.todolist.exception.application.TaskNotExistException;
 import hwicode.schedule.dailyschedule.todolist.infra.DailyToDoListRepository;
 import hwicode.schedule.dailyschedule.todolist.infra.TaskRepository;
 import hwicode.schedule.dailyschedule.todolist.presentation.task.dto.information_modify.TaskInformationModifyRequest;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static hwicode.schedule.dailyschedule.todolist.ToDoListDataHelper.TASK_NAME;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
@@ -50,5 +52,17 @@ class TaskServiceIntegrationTest {
         Task savedTask = taskRepository.findById(task.getId()).orElseThrow();
         assertThat(savedTask.changePriority(Priority.FIRST)).isFalse();
         assertThat(savedTask.changeImportance(Importance.FIRST)).isFalse();
+    }
+
+    @Test
+    void 존재하지_않는_과제를_조회하면_에러가_발생한다() {
+        //given
+        Long noneExistId = 1L;
+
+        TaskInformationModifyRequest taskInformationModifyRequest = new TaskInformationModifyRequest(Priority.FIRST, Importance.FIRST);
+
+        // when then
+        assertThatThrownBy(() -> taskService.changeTaskInformation(noneExistId, taskInformationModifyRequest))
+                .isInstanceOf(TaskNotExistException.class);
     }
 }
