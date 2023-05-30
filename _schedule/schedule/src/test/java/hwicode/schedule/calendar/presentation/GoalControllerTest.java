de128260c54494f8baee548f0bd13120948c1093
hwicode <khc3796@naver.com>
2023-05-30 14:34:50 +0900

    Test: GoalControllerTest에 GoalAggregateService에서 발생하는 커스텀 에러들의 예외 처리 테스트 추가 -#83

--- _schedule/schedule/src/test/java/hwicode/schedule/calendar/presentation/GoalControllerTest.java
+++ _schedule/schedule/src/test/java/hwicode/schedule/calendar/presentation/GoalControllerTest.java
@@ -4,15 +4,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import hwicode.schedule.calendar.application.GoalAggregateService;
 import hwicode.schedule.calendar.domain.GoalStatus;
 import hwicode.schedule.calendar.domain.SubGoalStatus;
+import hwicode.schedule.calendar.exception.domain.goal.*;
+import hwicode.schedule.calendar.exception.infra.GoalNotFoundException;
 import hwicode.schedule.calendar.presentation.goal.GoalController;
 import hwicode.schedule.calendar.presentation.goal.dto.calendargoal_delete.CalendarGoalDeleteRequest;
-import hwicode.schedule.calendar.presentation.goal.dto.subgoal_delete.SubGoalDeleteRequest;
 import hwicode.schedule.calendar.presentation.goal.dto.goal_status_modify.GoalStatusModifyRequest;
 import hwicode.schedule.calendar.presentation.goal.dto.goal_status_modify.GoalStatusModifyResponse;
-import hwicode.schedule.calendar.presentation.goal.dto.subgoal_save.SubGoalSaveRequest;
-import hwicode.schedule.calendar.presentation.goal.dto.subgoal_save.SubGoalSaveResponse;
+import hwicode.schedule.calendar.presentation.goal.dto.subgoal_delete.SubGoalDeleteRequest;
 import hwicode.schedule.calendar.presentation.goal.dto.subgoal_name_modify.SubGoalNameModifyRequest;
 import hwicode.schedule.calendar.presentation.goal.dto.subgoal_name_modify.SubGoalNameModifyResponse;
+import hwicode.schedule.calendar.presentation.goal.dto.subgoal_save.SubGoalSaveRequest;
+import hwicode.schedule.calendar.presentation.goal.dto.subgoal_save.SubGoalSaveResponse;
 import hwicode.schedule.calendar.presentation.goal.dto.subgoal_status_modify.SubGoalStatusModifyRequest;
 import hwicode.schedule.calendar.presentation.goal.dto.subgoal_status_modify.SubGoalStatusModifyResponse;
 import org.junit.jupiter.api.Test;
@@ -29,6 +31,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(GoalController.class)
@@ -176,6 +179,123 @@ class GoalControllerTest {
         perform.andExpect(status().isNoContent());
 
         verify(goalAggregateService).deleteCalendarGoal(any(), any());
+    }
+
+    @Test
+    void 목표의_삭제를_요청할_때_목표가_존재하지_않으면_에러가_발생한다() throws Exception {
+        // given
+        GoalNotFoundException goalNotFoundException = new GoalNotFoundException();
+        given(goalAggregateService.deleteGoal(any()))
+                .willThrow(goalNotFoundException);
+
+        // when
+        ResultActions perform = mockMvc.perform(
+                delete("/goals/{goalId}", GOAL_ID));
+
+        // then
+        perform.andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value(goalNotFoundException.getMessage()));
+    }
+
+    @Test
+    void 목표의_진행_상태_변경을_요청할_때_존재하지_않는_진행_상태면_에러가_발생한다() throws Exception {
+        // given
+        GoalStatusNotFoundException goalStatusNotFoundException = new GoalStatusNotFoundException();
+        given(goalAggregateService.changeGoalStatus(any(), any()))
+                .willThrow(goalStatusNotFoundException);
+
+        // when
+        ResultActions perform = mockMvc.perform(
+                patch("/goals/{goalId}/status", GOAL_ID)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                // 존재하지 않는 진행상태로 가정
+                                new GoalStatusModifyRequest(GoalStatus.DONE))
+                        ));
+
+        // then
+        perform.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(goalStatusNotFoundException.getMessage()));
+    }
+
+    @Test
+    void 서브_목표_생성을_요청할_때_서브_목표가_중복되면_에러가_발생한다() throws Exception {
+        // given
+        SubGoalDuplicateException subGoalDuplicateException = new SubGoalDuplicateException();
+        given(goalAggregateService.saveSubGoal(any(), any()))
+                .willThrow(subGoalDuplicateException);
+
+        // when
+        ResultActions perform = mockMvc.perform(
+                post("/goals/{goalId}/sub-goals", GOAL_ID)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                new SubGoalSaveRequest(SUB_GOAL_NAME)
+                        )));
+
+        // then
+        perform.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(subGoalDuplicateException.getMessage()));
+    }
+
+    @Test
+    void 목표의_진행_상태를_DONE으로_변경을_요청할_때_서브_목표가_모두_DONE이_아니면_에러가_발생한다() throws Exception {
+        // given
+        SubGoalNotAllDoneException subGoalNotAllDoneException = new SubGoalNotAllDoneException();
+        given(goalAggregateService.changeGoalStatus(any(), any()))
+                .willThrow(subGoalNotAllDoneException);
+
+        // when
+        ResultActions perform = mockMvc.perform(
+                patch("/goals/{goalId}/status", GOAL_ID)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                new GoalStatusModifyRequest(GoalStatus.DONE)
+                        )));
+
+        // then
+        perform.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(subGoalNotAllDoneException.getMessage()));
+    }
+
+    @Test
+    void 목표의_진행_상태를_TODO로_변경을_요청할_때_서브_목표가_모두_TODO가_아니면_에러가_발생한다() throws Exception {
+        // given
+        SubGoalNotAllTodoException subGoalNotAllTodoException = new SubGoalNotAllTodoException();
+        given(goalAggregateService.changeGoalStatus(any(), any()))
+                .willThrow(subGoalNotAllTodoException);
+
+        // when
+        ResultActions perform = mockMvc.perform(
+                patch("/goals/{goalId}/status", GOAL_ID)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                new GoalStatusModifyRequest(GoalStatus.TODO)
+                        )));
+
+        // then
+        perform.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(subGoalNotAllTodoException.getMessage()));
+    }
+
+    @Test
+    void 서브_목표의_삭제를_요청할_때_서브_목표가_존재하지_않으면_에러가_발생한다() throws Exception {
+        // given
+        SubGoalNotFoundException subGoalNotFoundException = new SubGoalNotFoundException();
+        given(goalAggregateService.deleteSubGoal(any(), any()))
+                .willThrow(subGoalNotFoundException);
+
+        // when
+        ResultActions perform = mockMvc.perform(
+                delete("/goals/{goalId}/sub-goals/{subGoalId}", GOAL_ID, SUB_GOAL_ID)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                new SubGoalDeleteRequest(SUB_GOAL_NAME)
+                        )));
+
+        // then
+        perform.andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value(subGoalNotFoundException.getMessage()));
     }
 
 }
