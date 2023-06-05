62200a40902e615bfd1bdd90b84b45ed2c450937
hwicode <khc3796@naver.com>
2023-06-06 00:37:11 +0900

    Test: CalendarValidTest 클래스에 @ParameterizedTest를 적용함 -#87

--- _schedule/schedule/src/test/java/hwicode/schedule/validation_annotation_test/calendar/CalendarValidTest.java
+++ _schedule/schedule/src/test/java/hwicode/schedule/validation_annotation_test/calendar/CalendarValidTest.java
@@ -7,6 +7,9 @@ import hwicode.schedule.calendar.presentation.calendar.dto.save.GoalSaveRequest;
 import hwicode.schedule.calendar.presentation.calendar.dto.weekly_study_date_modify.WeeklyStudyDateModifyRequest;
 import hwicode.schedule.common.exception.GlobalErrorCode;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -18,8 +21,10 @@ import javax.validation.constraints.Size;
 import java.time.YearMonth;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import static hwicode.schedule.calendar.CalendarDataHelper.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -103,22 +108,14 @@ class CalendarValidTest {
                 .andExpect(jsonPath("$.errors[0].message").value(message));
     }
 
-    @Test
-    void 일주일간_공부일이_0이면_통과된다() throws Exception {
-        // given
-        int weeklyStudyDate = 0;
-        WeeklyStudyDateModifyRequest weeklyStudyDateModifyRequest = new WeeklyStudyDateModifyRequest(YEAR_MONTH, weeklyStudyDate);
-
-        // when
-        ResultActions perform = mockMvc.perform(patch("/calendars/{calendarId}/weeklyStudyDate", CALENDAR_ID)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(weeklyStudyDateModifyRequest)));
-
-        // then
-        perform.andExpect(status().isOk());
+    private static Stream<Arguments> provideWeeklyStudyDate() {
+        return Stream.of(
+                arguments(0), arguments(1), arguments(2), arguments(3), arguments(4), arguments(5), arguments(6), arguments(7)
+        );
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("provideWeeklyStudyDate")
     void 일주일간_공부일이_0과_7사이면_통과된다() throws Exception {
         // given
         int weeklyStudyDate = 4;
@@ -133,31 +130,19 @@ class CalendarValidTest {
         perform.andExpect(status().isOk());
     }
 
-    @Test
-    void 일주일간_공부일이_오버되면_에러가_발생한다() throws Exception {
-        // given
-        int overDate = 8;
-        WeeklyStudyDateModifyRequest weeklyStudyDateModifyRequest = new WeeklyStudyDateModifyRequest(YEAR_MONTH, overDate);
-
-        // when
-        ResultActions perform = mockMvc.perform(patch("/calendars/{calendarId}/weeklyStudyDate", CALENDAR_ID)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(weeklyStudyDateModifyRequest)));
-
-        // then
-        String field = "weeklyStudyDate";
-        String message = "must be less than or equal to 7";
-        perform.andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.message").value(GlobalErrorCode.INVALID_PARAMETER.getMessage()))
-                .andExpect(jsonPath("$.errors[0].field").value(field))
-                .andExpect(jsonPath("$.errors[0].message").value(message));
+    private static Stream<Arguments> provideWrongWeeklyStudyDate() {
+        return Stream.of(
+                arguments(8, "must be less than or equal to 7"),
+                arguments(-1, "must be greater than or equal to 0"),
+                arguments(null, "must not be null")
+        );
     }
 
-    @Test
-    void 일주일간_공부일이_0보다_작으면_에러가_발생한다() throws Exception {
+    @ParameterizedTest
+    @MethodSource("provideWrongWeeklyStudyDate")
+    void 일주일간_공부일에_잘못된_값이_들어오면_에러가_발생한다(Integer weeklyStudyDate, String errorMessage) throws Exception {
         // given
-        int wrongDate = -1;
-        WeeklyStudyDateModifyRequest weeklyStudyDateModifyRequest = new WeeklyStudyDateModifyRequest(YEAR_MONTH, wrongDate);
+        WeeklyStudyDateModifyRequest weeklyStudyDateModifyRequest = new WeeklyStudyDateModifyRequest(YEAR_MONTH, weeklyStudyDate);
 
         // when
         ResultActions perform = mockMvc.perform(patch("/calendars/{calendarId}/weeklyStudyDate", CALENDAR_ID)
@@ -166,30 +151,10 @@ class CalendarValidTest {
 
         // then
         String field = "weeklyStudyDate";
-        String message = "must be greater than or equal to 0";
         perform.andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.message").value(GlobalErrorCode.INVALID_PARAMETER.getMessage()))
                 .andExpect(jsonPath("$.errors[0].field").value(field))
-                .andExpect(jsonPath("$.errors[0].message").value(message));
-    }
-
-    @Test
-    void 일주일간_공부일이_존재하지_않으면_에러가_발생한다() throws Exception {
-        // given
-        WeeklyStudyDateModifyRequest weeklyStudyDateModifyRequest = new WeeklyStudyDateModifyRequest(YEAR_MONTH, null);
-
-        // when
-        ResultActions perform = mockMvc.perform(patch("/calendars/{calendarId}/weeklyStudyDate", CALENDAR_ID)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(weeklyStudyDateModifyRequest)));
-
-        // then
-        String field = "weeklyStudyDate";
-        String message = "must not be null";
-        perform.andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.message").value(GlobalErrorCode.INVALID_PARAMETER.getMessage()))
-                .andExpect(jsonPath("$.errors[0].field").value(field))
-                .andExpect(jsonPath("$.errors[0].message").value(message));
+                .andExpect(jsonPath("$.errors[0].message").value(errorMessage));
     }
 
 }
