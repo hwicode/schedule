88865670632da9de92854d092da28ed97aa6e4fe
hwicode <khc3796@naver.com>
2023-06-05 22:48:53 +0900

    Test: @Valid 테스트에서 @NotNull, @PositiveOrZero, @Max(7)가 붙은 Integer 필드 테스트를 CalendarValidTest에서 진행함 -#87

--- _schedule/schedule/src/test/java/hwicode/schedule/validation_annotation_test/calendar/CalendarValidTest.java
+++ _schedule/schedule/src/test/java/hwicode/schedule/validation_annotation_test/calendar/CalendarValidTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import hwicode.schedule.calendar.application.CalendarAggregateService;
 import hwicode.schedule.calendar.presentation.calendar.CalendarController;
 import hwicode.schedule.calendar.presentation.calendar.dto.save.GoalSaveRequest;
+import hwicode.schedule.calendar.presentation.calendar.dto.weekly_study_date_modify.WeeklyStudyDateModifyRequest;
 import hwicode.schedule.common.exception.GlobalErrorCode;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +19,8 @@ import java.time.YearMonth;
 import java.util.HashSet;
 import java.util.Set;
 
-import static hwicode.schedule.calendar.CalendarDataHelper.GOAL_NAME;
-import static hwicode.schedule.calendar.CalendarDataHelper.YEAR_MONTH;
+import static hwicode.schedule.calendar.CalendarDataHelper.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -81,6 +82,95 @@ class CalendarValidTest {
         // then
         String field = "yearMonths";
         String message = GoalSaveRequest.class.getDeclaredField(field).getAnnotation(Size.class).message();
+        perform.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(GlobalErrorCode.INVALID_PARAMETER.getMessage()))
+                .andExpect(jsonPath("$.errors[0].field").value(field))
+                .andExpect(jsonPath("$.errors[0].message").value(message));
+    }
+
+    @Test
+    void 일주일간_공부일이_0이면_통과된다() throws Exception {
+        // given
+        int weeklyStudyDate = 0;
+        WeeklyStudyDateModifyRequest weeklyStudyDateModifyRequest = new WeeklyStudyDateModifyRequest(YEAR_MONTH, weeklyStudyDate);
+
+        // when
+        ResultActions perform = mockMvc.perform(patch("/calendars/{calendarId}/weeklyStudyDate", CALENDAR_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(weeklyStudyDateModifyRequest)));
+
+        // then
+        perform.andExpect(status().isOk());
+    }
+
+    @Test
+    void 일주일간_공부일이_0과_7사이면_통과된다() throws Exception {
+        // given
+        int weeklyStudyDate = 4;
+        WeeklyStudyDateModifyRequest weeklyStudyDateModifyRequest = new WeeklyStudyDateModifyRequest(YEAR_MONTH, weeklyStudyDate);
+
+        // when
+        ResultActions perform = mockMvc.perform(patch("/calendars/{calendarId}/weeklyStudyDate", CALENDAR_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(weeklyStudyDateModifyRequest)));
+
+        // then
+        perform.andExpect(status().isOk());
+    }
+
+    @Test
+    void 일주일간_공부일이_오버되면_에러가_발생한다() throws Exception {
+        // given
+        int overDate = 8;
+        WeeklyStudyDateModifyRequest weeklyStudyDateModifyRequest = new WeeklyStudyDateModifyRequest(YEAR_MONTH, overDate);
+
+        // when
+        ResultActions perform = mockMvc.perform(patch("/calendars/{calendarId}/weeklyStudyDate", CALENDAR_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(weeklyStudyDateModifyRequest)));
+
+        // then
+        String field = "weeklyStudyDate";
+        String message = "must be less than or equal to 7";
+        perform.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(GlobalErrorCode.INVALID_PARAMETER.getMessage()))
+                .andExpect(jsonPath("$.errors[0].field").value(field))
+                .andExpect(jsonPath("$.errors[0].message").value(message));
+    }
+
+    @Test
+    void 일주일간_공부일이_0보다_작으면_에러가_발생한다() throws Exception {
+        // given
+        int wrongDate = -1;
+        WeeklyStudyDateModifyRequest weeklyStudyDateModifyRequest = new WeeklyStudyDateModifyRequest(YEAR_MONTH, wrongDate);
+
+        // when
+        ResultActions perform = mockMvc.perform(patch("/calendars/{calendarId}/weeklyStudyDate", CALENDAR_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(weeklyStudyDateModifyRequest)));
+
+        // then
+        String field = "weeklyStudyDate";
+        String message = "must be greater than or equal to 0";
+        perform.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(GlobalErrorCode.INVALID_PARAMETER.getMessage()))
+                .andExpect(jsonPath("$.errors[0].field").value(field))
+                .andExpect(jsonPath("$.errors[0].message").value(message));
+    }
+
+    @Test
+    void 일주일간_공부일이_존재하지_않으면_에러가_발생한다() throws Exception {
+        // given
+        WeeklyStudyDateModifyRequest weeklyStudyDateModifyRequest = new WeeklyStudyDateModifyRequest(YEAR_MONTH, null);
+
+        // when
+        ResultActions perform = mockMvc.perform(patch("/calendars/{calendarId}/weeklyStudyDate", CALENDAR_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(weeklyStudyDateModifyRequest)));
+
+        // then
+        String field = "weeklyStudyDate";
+        String message = "must not be null";
         perform.andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.message").value(GlobalErrorCode.INVALID_PARAMETER.getMessage()))
                 .andExpect(jsonPath("$.errors[0].field").value(field))
