7a9dc5b69cf3a62afc4cdc502bf4f2ed813c3505
hwicode <khc3796@naver.com>
2023-06-12 18:17:35 +0900

    Refactor: INTERNAL_SERVER_ERROR의 에러 메시지 수정 -#88

--- _schedule/schedule/src/main/java/hwicode/schedule/common/exception/GlobalErrorCode.java
+++ _schedule/schedule/src/main/java/hwicode/schedule/common/exception/GlobalErrorCode.java
@@ -11,7 +11,7 @@ public enum GlobalErrorCode {
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "유효하지 않는 파라미터가 있습니다."),
     INVALID_MESSAGE_BODY_TYPE(HttpStatus.BAD_REQUEST, "HTTP message body 중에 타입을 잘못 설정한게 있습니다."),
 
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에서 에러가 발생하였습니다"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "요청을 처리하는 동안 서버에서 오류가 발생했습니다. 나중에 다시 시도해주세요."),
     NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 URL입니다.");
 
     private final HttpStatus httpStatus;
