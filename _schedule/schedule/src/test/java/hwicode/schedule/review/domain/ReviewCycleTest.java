38f7c04b84b942b264e7090ad18f50088b796bf4
hwicode <khc3796@naver.com>
2023-06-23 17:19:44 +0900

    Refactor: ReviewCycle를 review.doamin 패키지로 옮김 -#91

--- _schedule/schedule/src/test/java/hwicode/schedule/review/domain/ReviewCycleTest.java
+++ _schedule/schedule/src/test/java/hwicode/schedule/review/domain/ReviewCycleTest.java
@@ -1,4 +1,4 @@
-package hwicode.schedule.review;
+package hwicode.schedule.review.domain;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -150,49 +150,6 @@ class ReviewCycleTest {
 
         // then
         assertThat(reviewCycle.getCycle()).hasSize(3);
-    }
-
-}
-
-class ReviewCycle {
-
-    private String name;
-    private List<Integer> reviewCycleDates;
-
-    public ReviewCycle(String name, List<Integer> reviewCycleDates) {
-        validateReviewCycleDates(reviewCycleDates);
-        this.name = name;
-        this.reviewCycleDates = new ArrayList<>(reviewCycleDates);
-    }
-
-    private void validateReviewCycleDates(List<Integer> reviewCycleDates) {
-        for (Integer reviewCycleDate : reviewCycleDates) {
-            if (reviewCycleDate == null) {
-                throw new IllegalArgumentException();
-            }
-
-            if (reviewCycleDate < 1 || reviewCycleDate > 60) {
-                throw new IllegalArgumentException();
-            }
-        }
-    }
-
-    public boolean changeName(String name) {
-        if (this.name.equals(name)) {
-            return false;
-        }
-        this.name = name;
-        return true;
-    }
-
-    public List<Integer> changeCycle(List<Integer> newReviewCycleDates) {
-        validateReviewCycleDates(newReviewCycleDates);
-        this.reviewCycleDates = new ArrayList<>(newReviewCycleDates);
-        return new ArrayList<>(newReviewCycleDates);
-    }
-
-    public List<Integer> getCycle() {
-        return new ArrayList<>(reviewCycleDates);
     }
 
 }
