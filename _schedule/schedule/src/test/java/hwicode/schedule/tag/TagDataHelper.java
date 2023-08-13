14c9aa341ba62308a4b0bc65512f73eeb11a5231
hwicode <khc3796@naver.com>
2023-08-13 14:26:15 +0900

    Feature: Tag를 생성하고 Tag의 이름을 수정하는 기능 구현함 -#111

---
+++ _schedule/schedule/src/test/java/hwicode/schedule/tag/TagDataHelper.java
@@ -0,0 +1,7 @@
+package hwicode.schedule.tag;
+
+public class TagDataHelper {
+
+    public static final String TAG_NAME = "tagName";
+    public static final String NEW_TAG_NAME = "newTagName";
+}
---
+++ _schedule/schedule/src/test/java/hwicode/schedule/tag/domain/TagTest.java
@@ -0,0 +1,53 @@
+package hwicode.schedule.tag.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static hwicode.schedule.tag.TagDataHelper.NEW_TAG_NAME;
+import static hwicode.schedule.tag.TagDataHelper.TAG_NAME;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TagTest {
+
+    @Test
+    void 태그의_이름을_수정할_때_이름이_동일하면_변경이_없으므로_false가_리턴된다() {
+        // given
+        Tag tag = new Tag(TAG_NAME);
+
+        // when
+        boolean isChange = tag.changeName(TAG_NAME);
+
+        // then
+        assertThat(isChange).isFalse();
+    }
+
+    @Test
+    void 태그의_이름을_수정할_때_이름이_다르면_변경이_있으므로_true가_리턴된다() {
+        // given
+        Tag tag = new Tag(TAG_NAME);
+
+        // when
+        boolean isChange = tag.changeName(NEW_TAG_NAME);
+
+        // then
+        assertThat(isChange).isTrue();
+    }
+
+}
+
+class Tag {
+
+    private String name;
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public boolean changeName(String name) {
+        if (this.name.equals(name)) {
+            return false;
+        }
+        this.name = name;
+        return true;
+    }
+
+}
