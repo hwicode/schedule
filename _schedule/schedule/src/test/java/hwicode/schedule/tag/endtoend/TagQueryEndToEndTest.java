cf16302bb9f0ebb46eb84ce81606cd981a5e6f15
hwicode <khc3796@naver.com>
2023-08-27 20:42:48 +0900

    Test: 태그로 계획표를 검색하는 기능 엔드 투 엔드 테스트 구현함 -#123

---
+++ _schedule/schedule/src/test/java/hwicode/schedule/tag/endtoend/TagQueryEndToEndTest.java
@@ -0,0 +1,48 @@
+package hwicode.schedule.tag.endtoend;
+
+import hwicode.schedule.DatabaseCleanUp;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+import static hwicode.schedule.tag.TagDataHelper.DAILY_TAG_LIST_ID;
+import static hwicode.schedule.tag.TagDataHelper.TAG_ID;
+import static io.restassured.RestAssured.given;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class TagQueryEndToEndTest {
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    DatabaseCleanUp databaseCleanUp;
+
+    @BeforeEach
+    void clearDatabase() {
+        databaseCleanUp.execute();
+    }
+
+    @Test
+    void 특정_태그를_가지고_있는_계획표들_조회_요청() {
+        // given
+        RequestSpecification requestSpecification = given().port(port)
+                .queryParam("tagId", TAG_ID)
+                .queryParam("lastDailyTagListId", DAILY_TAG_LIST_ID);
+
+        // when
+        Response response = requestSpecification.when()
+                .get("/daily-tag-lists");
+
+        // then
+        response.then()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+}
