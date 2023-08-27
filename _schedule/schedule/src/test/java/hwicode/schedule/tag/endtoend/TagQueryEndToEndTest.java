2e62eca517f8d8b875d7aa825f53c516b4009fd2
hwicode <khc3796@naver.com>
2023-08-27 21:21:20 +0900

    Test: 태그로 메모를 검색하는 기능 엔드 투 엔드 테스트 구현함 -#123

--- _schedule/schedule/src/test/java/hwicode/schedule/tag/endtoend/TagQueryEndToEndTest.java
+++ _schedule/schedule/src/test/java/hwicode/schedule/tag/endtoend/TagQueryEndToEndTest.java
@@ -11,8 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 
-import static hwicode.schedule.tag.TagDataHelper.DAILY_TAG_LIST_ID;
-import static hwicode.schedule.tag.TagDataHelper.TAG_ID;
+import static hwicode.schedule.tag.TagDataHelper.*;
 import static io.restassured.RestAssured.given;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -39,6 +38,22 @@ class TagQueryEndToEndTest {
         // when
         Response response = requestSpecification.when()
                 .get("/daily-tag-lists");
+
+        // then
+        response.then()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    void 특정_태그를_가지고_있는_메모들_조회_요청() {
+        // given
+        RequestSpecification requestSpecification = given().port(port)
+                .queryParam("tagId", TAG_ID)
+                .queryParam("lastMemoId", MEMO_ID);
+
+        // when
+        Response response = requestSpecification.when()
+                .get("/memos");
 
         // then
         response.then()
