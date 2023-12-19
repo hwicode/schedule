8575a435f422e9c5d702a6f5a8b2e507c03c4213
hwicode <khc3796@naver.com>
2023-12-19 14:24:36 +0900

    Refactor: Emoji 의 코드를 없앰 -#35

--- _schedule/schedule/src/main/java/hwicode/schedule/dailyschedule/shared_domain/Emoji.java
+++ _schedule/schedule/src/main/java/hwicode/schedule/dailyschedule/shared_domain/Emoji.java
@@ -1,21 +1,5 @@
 package hwicode.schedule.dailyschedule.shared_domain;
 
 public enum Emoji {
-    // 😁
-    GOOD("\uD83D\uDE01"),
-    // 🙂
-    NOT_BAD("\uD83D\uDE42"),
-    // 😟
-    BAD("\uD83D\uDE1F");
-
-    // utf-16
-    private final String code;
-
-    Emoji(String code) {
-        this.code = code;
-    }
-
-    public String getCode() {
-        return code;
-    }
+    GOOD, NOT_BAD, BAD;
 }
