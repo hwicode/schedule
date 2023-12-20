a7b5c3f05a7fd11d4c1da03bf3317e0b11995c4d
hwicode <khc3796@naver.com>
2023-12-21 02:09:22 +0900

    Test: checklist의 과제체커를 생성하면, 과제의 긴급도와 중요도도 저장되는지 확인하는 테스트 추가함 -#133

---
+++ _schedule/schedule/src/test/java/hwicode/schedule/cross_boundedcontext_test/ChecklistAndToDoListTest.java
@@ -0,0 +1,73 @@
+package hwicode.schedule.cross_boundedcontext_test;
+
+import hwicode.schedule.DatabaseCleanUp;
+import hwicode.schedule.dailyschedule.checklist.application.dailychecklist_aggregate_service.TaskCheckerSubService;
+import hwicode.schedule.dailyschedule.checklist.domain.DailyChecklist;
+import hwicode.schedule.dailyschedule.checklist.infra.jpa_repository.DailyChecklistRepository;
+import hwicode.schedule.dailyschedule.checklist.presentation.taskchecker.dto.save.TaskSaveRequest;
+import hwicode.schedule.dailyschedule.shared_domain.Difficulty;
+import hwicode.schedule.dailyschedule.shared_domain.Importance;
+import hwicode.schedule.dailyschedule.shared_domain.Priority;
+import hwicode.schedule.dailyschedule.todolist.domain.Task;
+import hwicode.schedule.dailyschedule.todolist.infra.jpa_repository.TaskRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.Stream;
+
+import static hwicode.schedule.dailyschedule.checklist.ChecklistDataHelper.TASK_CHECKER_NAME;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@SpringBootTest
+class ChecklistAndToDoListTest {
+
+    @Autowired
+    DatabaseCleanUp databaseCleanUp;
+
+    @Autowired
+    TaskCheckerSubService taskCheckerSubService;
+
+    @Autowired
+    DailyChecklistRepository dailyChecklistRepository;
+
+    @Autowired
+    TaskRepository taskRepository;
+
+    @BeforeEach
+    void clearDatabase() {
+        databaseCleanUp.execute();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTaskInformation")
+    void 과제체커를_생성하면_과제의_긴급도와_중요도도_저장된다(Priority priority, Importance importance) {
+        // given
+        DailyChecklist dailyChecklist = new DailyChecklist();
+        dailyChecklistRepository.save(dailyChecklist);
+
+        TaskSaveRequest taskSaveRequest = new TaskSaveRequest(dailyChecklist.getId(), TASK_CHECKER_NAME, Difficulty.NORMAL, priority, importance);
+
+        // when
+        Long taskCheckerId = taskCheckerSubService.saveTaskChecker(taskSaveRequest);
+
+        // then
+        Task task = taskRepository.findById(taskCheckerId).orElseThrow();
+        assertThat(task.changePriority(priority)).isFalse();
+        assertThat(task.changeImportance(importance)).isFalse();
+    }
+
+    private static Stream<Arguments> provideTaskInformation() {
+        return Stream.of(
+                arguments(Priority.FIRST, Importance.THIRD),
+                arguments(Priority.FIRST, Importance.FIRST),
+                arguments(Priority.SECOND, Importance.SECOND),
+                arguments(Priority.THIRD, Importance.THIRD)
+        );
+    }
+
+}
