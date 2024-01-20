4b4032b74b59eb0d515443c1668774b33721f06f
hwicode <khc3796@naver.com>
2024-01-20 17:32:35 +0900

    Test: OauthIdTokenDecoder 클래스에 테스트 코드를 추가함 -#134

---
+++ _schedule/schedule/src/test/java/hwicode/schedule/auth/infra/OauthIdTokenDecoderTest.java
@@ -0,0 +1,48 @@
+package hwicode.schedule.auth.infra;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hwicode.schedule.auth.exception.infra.OauthIdTokenException;
+import hwicode.schedule.auth.infra.client.OauthIdTokenDecoder;
+import io.jsonwebtoken.Jwts;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class OauthIdTokenDecoderTest {
+
+    @Test
+    void id토큰을_디코드할_수_있다() {
+        // given
+        String name = "name";
+        String email = "email";
+        String jwt = Jwts.builder()
+                .claim(name, name)
+                .claim(email, email)
+                .compact();
+
+        OauthIdTokenDecoder oauthIdTokenDecoder = new OauthIdTokenDecoder(new ObjectMapper());
+
+        // when
+        Map<String, String> decode = oauthIdTokenDecoder.decode(jwt);
+
+        // then
+        assertThat(decode)
+                .containsEntry(name, name)
+                .containsEntry(email, email);
+    }
+
+    @Test
+    void 잘못된_id토큰을_디코드하면_에러가_발생한다() {
+        // given
+        String jwt = "dfim.idf";
+        OauthIdTokenDecoder oauthIdTokenDecoder = new OauthIdTokenDecoder(new ObjectMapper());
+
+        // when then
+        assertThatThrownBy(() -> oauthIdTokenDecoder.decode(jwt))
+                .isInstanceOf(OauthIdTokenException.class);
+    }
+
+}
