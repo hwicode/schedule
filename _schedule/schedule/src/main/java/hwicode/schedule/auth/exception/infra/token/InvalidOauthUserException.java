f69977cbf2a633c1dd2d48cf5e04980fd14f0322
hwicode <khc3796@naver.com>
2024-03-04 17:16:12 +0900

    Fix: InvalidTokenException, InvalidOauthUserException의 httpStatus를 401로 수정함 -#134

--- _schedule/schedule/src/main/java/hwicode/schedule/auth/exception/infra/token/InvalidOauthUserException.java
+++ _schedule/schedule/src/main/java/hwicode/schedule/auth/exception/infra/token/InvalidOauthUserException.java
@@ -6,6 +6,6 @@ import org.springframework.http.HttpStatus;
 public class InvalidOauthUserException extends BusinessException {
 
     public InvalidOauthUserException() {
-        super("유저의 id가 없어서 토큰이 유효하지 않습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+        super("유저의 id가 없어서 토큰이 유효하지 않습니다.", HttpStatus.UNAUTHORIZED);
     }
 }
--- _schedule/schedule/src/main/java/hwicode/schedule/auth/exception/infra/token/InvalidTokenException.java
+++ _schedule/schedule/src/main/java/hwicode/schedule/auth/exception/infra/token/InvalidTokenException.java
@@ -6,6 +6,6 @@ import org.springframework.http.HttpStatus;
 public class InvalidTokenException extends BusinessException {
 
     public InvalidTokenException() {
-        super("토큰이 유효하지 않습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+        super("토큰이 유효하지 않습니다.", HttpStatus.UNAUTHORIZED);
     }
 }
