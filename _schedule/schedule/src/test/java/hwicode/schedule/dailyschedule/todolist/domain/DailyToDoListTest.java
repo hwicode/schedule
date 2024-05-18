adfd9a2688feeee4b0e17315123d65850709c32a
hwicode <khc3796@naver.com>
2024-05-18 16:44:21 +0900

    Fix: DailyToDoList 엔티티의 review를 변경할 때, null값도 처리할 수 있게 바꿈 -#145
    - DailySchedule을 생성할 때, review 컬럼이 없기에 기본값으로 저장해야한다.
    - MySQL에서 TEXT타입의 기본값을 설정할 수 없음
    - review에서 null 값을 기본값으로 사용함

--- _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/todolist/domain/DailyToDoListTest.java
+++ _schedule/schedule/src/test/java/hwicode/schedule/dailyschedule/todolist/domain/DailyToDoListTest.java
@@ -39,6 +39,31 @@ class DailyToDoListTest {
     }
 
     @Test
+    void 리뷰가_null일_때_null인_리뷰를_작성하면_리뷰에_변경이_없으므로_false가_리턴된다() {
+        // given
+        DailyToDoList dailyToDoList = new DailyToDoList(Emoji.NOT_BAD, 1L);
+
+        // when
+        boolean isChange = dailyToDoList.writeReview(null);
+
+        // then
+        assertThat(isChange).isFalse();
+    }
+
+    @Test
+    void 리뷰가_null일_때_새로운_리뷰를_작성하면_리뷰에_변경이_있으므로_true가_리턴된다() {
+        // given
+        DailyToDoList dailyToDoList = new DailyToDoList(Emoji.NOT_BAD, 1L);
+        String review = "나쁘지 않은데!";
+
+        // when
+        boolean isChange = dailyToDoList.writeReview(review);
+
+        // then
+        assertThat(isChange).isTrue();
+    }
+
+    @Test
     void 동일한_이모지로_변경하면_이모지에_변경이_없으므로_false가_리턴된다() {
         // given
         DailyToDoList dailyToDoList = new DailyToDoList(Emoji.NOT_BAD, 1L);
